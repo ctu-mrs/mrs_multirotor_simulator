66b6e640f046c6ae5effe3f0c36bd1ed11bed7d9
Tomas Baca <klaxalk@gmail.com>
2023-01-30 22:34:43 +0100

    updated group mixing

--- src/controllers/mixer.cpp
+++ src/controllers/mixer.cpp
@@ -13,15 +13,26 @@ void Mixer::setParams(const Params& params) {
 
   Eigen::MatrixXd allocation_tmp = params.allocation_matrix;
 
-  for (int i = 0; i < allocation_tmp.rows(); i++) {
+  // | ---------------- normalized the third row ---------------- |
 
+  {
     double sum_abs = 0;
-
     for (int j = 0; j < allocation_tmp.cols(); j++) {
-      sum_abs += abs(params.allocation_matrix(i, j));
+      sum_abs += abs(params.allocation_matrix(2, j));
     }
 
-    allocation_tmp.row(i) = allocation_tmp.row(i) / sum_abs;
+    allocation_tmp.row(2) = allocation_tmp.row(2) / (0.5 * sum_abs * allocation_tmp.cols());
+  }
+
+  // | ----------------- normalize the forth row ---------------- |
+
+  {
+    double sum_abs = 0;
+    for (int j = 0; j < allocation_tmp.cols(); j++) {
+      sum_abs += abs(params.allocation_matrix(3, j));
+    }
+
+    allocation_tmp.row(3) = allocation_tmp.row(3) / (allocation_tmp.cols());
   }
 
   allocation_matrix_inv_ = allocation_tmp.transpose() * (allocation_tmp * allocation_tmp.transpose()).inverse();
@@ -36,12 +47,43 @@ reference::Actuators Mixer::getControlSignal(const reference::ControlGroup& refe
 
   actuators.motors = allocation_matrix_inv_ * ctrl_group;
 
-  // TODO: implement desaturation
+  // find minimum
+  double min = std::numeric_limits<double>::max();
+
   for (int i = 0; i < params_.n_motors; i++) {
-    if (actuators.motors[i] > 1.0) {
-      actuators.motors[i] = 1.0;
-    } else if (actuators.motors[i] < 0.0) {
-      actuators.motors[i] = 0.0;
+    if (actuators.motors[i] < min) {
+      min = actuators.motors[i];
+    }
+  }
+
+  if (min < 0.0) {
+    for (int i = 0; i < params_.n_motors; i++) {
+      actuators.motors[i] += abs(min);
+    }
+  }
+
+  // find maximum
+  double max = std::numeric_limits<double>::lowest();
+
+  for (int i = 0; i < params_.n_motors; i++) {
+    if (actuators.motors[i] > max) {
+      max = actuators.motors[i];
+    }
+  }
+
+  if (max > 1.0) {
+
+    if (reference.throttle > 1e-2) {
+      for (int i = 0; i < 3; i++) {
+        ctrl_group(i) = ctrl_group(i) / (actuators.motors.mean() / reference.throttle);
+      }
+
+      actuators.motors = allocation_matrix_inv_ * ctrl_group;
+
+    } else {
+      for (int i = 0; i < params_.n_motors; i++) {
+        actuators.motors[i] /= max;
+      }
     }
   }
 
