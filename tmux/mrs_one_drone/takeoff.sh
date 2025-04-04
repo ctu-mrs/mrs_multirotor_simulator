180284e11060422f79430c35d65f2d54beb00452
Tomas Baca <Tomas Baca>
2025-04-04 09:02:22 +0200

    updated session

--- tmux/mrs_one_drone/takeoff.sh
+++ tmux/mrs_one_drone/takeoff.sh
@@ -2,14 +2,8 @@ echo "arming"
 
 ros2 service call /uav1/hw_api/arming std_srvs/srv/SetBool '{"data": true}' 
 
-echo "toggling output"
-
-ros2 service call /uav1/control_manager/toggle_output std_srvs/srv/SetBool '{"data": true}' 
+sleep 1.0
 
 echo "toggling offboard"
 
 ros2 service call /uav1/hw_api/offboard std_srvs/srv/Trigger '{}' 
-
-echo "taking off"
-
-ros2 service call /uav1/uav_manager/takeoff std_srvs/srv/Trigger '{}' 
